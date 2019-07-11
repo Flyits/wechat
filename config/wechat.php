34b770998dc0a0649ff6f5e6e4d699125caeea60
flyits <1424071037@qq.com>
2019-07-11 13:33:22 +0800

    自动加载配置文件测试

---
+++ config/wechat.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flyits
+ * Date: 2019/7/11
+ * Time: 10:39
+ */
+
+return [
+	'app_id'     => 'wxf3c5912daa7446ff',
+	'app_secret' => '3a282400c06f37c7b8463c457fbdf5b0',
+];
---
+++ src/AccessToken.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flyits
+ * Date: 2019/7/11
+ * Time: 10:48
+ */
+declare(strict_types = 1);
+
+namespace flyits\wechat;
+
+
+class AccessToken
+{
+	/**
+	 * @var
+	 */
+	protected $config = [];
+	
+	protected function accessToken()
+	{
+	
+	}
+	
+}
